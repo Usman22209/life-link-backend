733448813cc89ff0eab452e5290a1c6b965bd1b2
Usman22209 <166890135+Usman22209@users.noreply.github.com>
2025-11-16 20:53:03 +0500

    basic supabase setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { SupabaseModule } from './supabase/supabase.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [SupabaseModule,ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+  
---
+++ src/supabase/supabase.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable()
+export class SupabaseService {
+  public supabase: SupabaseClient;
+
+  constructor() {
+    const url = process.env.SUPABASE_URL;
+    const key = process.env.SUPABASE_ANON_KEY;
+
+    if (!url || !key) {
+      throw new Error('Missing SUPABASE_URL or SUPABASE_ANON_KEY environment variables.');
+    }
+
+    this.supabase = createClient(url, key);
+  }
+}
