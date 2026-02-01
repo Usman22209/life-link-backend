3429fd877129e72566d69f71eab4ca209c0ef471
Usman22209 <166890135+Usman22209@users.noreply.github.com>
2026-02-01 18:56:11 +0500

    fixes

--- src/donation/dto/accept-donation.dto.ts
+++ src/donation/dto/accept-donation.dto.ts
@@ -1,8 +1,10 @@
 import { IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class AcceptDonationDto {
     @ApiProperty({ description: 'The ID of the blood request to accept' })
-    @IsUUID()
+    @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
+    @IsUUID('all')
     request_id: string;
 }
